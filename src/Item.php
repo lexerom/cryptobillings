df3b8e20ab2170ab4d918947b60909c18f380e3c
Alexey Suhih <lexerom@gmail.com>
2017-12-21 17:42:52 +0300

    Fixing Item -> remove constructor

--- src/Item.php
+++ src/Item.php
@@ -5,14 +5,6 @@ class Item
 {
     public $description;
     public $price;
-    public $currency;
-    public $quantity;
-    
-    public function __construct($description, $price, $currency, $quantity = 1)
-    {
-        $this->description = $description;
-        $this->price = $price;
-        $this->currency = $currency;
-        $this->quantity = $quantity;
-    }
+    public $currency = 'USD';
+    public $quantity = 1;
 }
